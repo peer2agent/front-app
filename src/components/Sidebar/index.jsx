eaccd7954184ed3a13d6c6cd75775d73227eb957
Gabriel Farias <gabrielfalves2003@gmail.com>
2025-02-21 09:21:25 -0300

    [add] sidebar component

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { LayoutDashboard, Wallet } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import DashboardSection from "@/components/DashboardSection";
+import { Divider, ImageContainer } from "./styles";
+
+export default function Sidebar() {
+    const router = useRouter();
+
+    return (
+        <DashboardSection
+            width="90px"
+            justifyContent="flex-start"
+            flexDirection="column"
+        >
+            <ImageContainer>
+                <img src="/images/peer2agent_logo.png" />
+            </ImageContainer>
+
+            <Divider />
+
+            <LayoutDashboard
+                size={24}
+                onClick={() => router.push("/dashboard")}
+            />
+
+            <Divider />
+
+            <Wallet size={24} onClick={() => router.push("/wallet")} />
+        </DashboardSection>
+    );
+}
